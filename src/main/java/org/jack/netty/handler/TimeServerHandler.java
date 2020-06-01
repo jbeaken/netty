82e786ead17ee12a45abaf954a298340457c7425
jbeaken <jack747@gmail.com>
2020-06-01 10:15:47 +0000

    Moving ByteBuf -> UnixTime

--- src/main/java/org/jack/netty/handler/TimeServerHandler.java
+++ src/main/java/org/jack/netty/handler/TimeServerHandler.java
@@ -1,23 +1,17 @@
 package org.jack.netty.handler;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.jack.netty.pojo.UnixTime;
 
 public class TimeServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
-    public void channelActive(final ChannelHandlerContext ctx) { 
-        final ByteBuf time = ctx.alloc().buffer(4); 
-        time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
-
-        final ChannelFuture f = ctx.writeAndFlush(time); 
-        f.addListener((ChannelFutureListener) future -> {
-            assert f == future;
-            ctx.close();
-        });
+    public void channelActive(final ChannelHandlerContext ctx) {
+        ChannelFuture f = ctx.writeAndFlush(new UnixTime());
+        f.addListener(ChannelFutureListener.CLOSE);
     }
 
     @Override
