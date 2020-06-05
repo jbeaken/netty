f03c428ba921a98cb59a69cd1897ab786ce3d959
jbeaken <jack747@gmail.com>
2020-06-05 10:15:47 +0000

    Back to non unix time

--- src/main/java/org/jack/netty/handler/TimeClientHandler.java
+++ src/main/java/org/jack/netty/handler/TimeClientHandler.java
@@ -1,19 +1,28 @@
 package org.jack.netty.handler;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import lombok.extern.slf4j.Slf4j;
 import org.jack.netty.pojo.UnixTime;
 
+import java.util.Date;
 
-@Slf4j
 public class TimeClientHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        UnixTime unixTime = (UnixTime) msg;
-        log.info("Time : {}", unixTime);
-        System.out.println(unixTime);
-        ctx.close();
+        ByteBuf m = (ByteBuf) msg; // (1)
+        try {
+            long currentTimeMillis = (m.readUnsignedInt() - 2208988800L) * 1000L;
+            System.out.println(new Date(currentTimeMillis));
+            ctx.close();
+        } finally {
+            m.release();
+        }
+
+//        UnixTime unixTime = (UnixTime) msg;
+//        log.info("Time : {}", unixTime);
+//        System.out.println(unixTime);
+//        ctx.close();
     }
 }
