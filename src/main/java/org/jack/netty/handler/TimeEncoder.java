f9292f6533399cac9b9626e16416fcd0f269d252
jbeaken <jack747@gmail.com>
2020-07-22 10:15:47 +0000

    Using MessageToByteEncoder

--- src/main/java/org/jack/netty/handler/TimeEncoder.java
+++ src/main/java/org/jack/netty/handler/TimeEncoder.java
@@ -2,20 +2,12 @@ package org.jack.netty.handler;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelOutboundHandlerAdapter;
-import io.netty.channel.ChannelPromise;
+import io.netty.handler.codec.MessageToByteEncoder;
 import org.jack.netty.pojo.UnixTime;
 
-public class TimeEncoder extends ChannelOutboundHandlerAdapter {
-
-        @Override
-        public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
-            UnixTime m = (UnixTime) msg;
-
-            ByteBuf encoded = ctx.alloc().buffer(4);
-
-            encoded.writeInt((int)m.value());
-
-            ctx.write(encoded, promise);
-        }
+public class TimeEncoder extends MessageToByteEncoder<UnixTime> {
+    @Override
+    protected void encode(ChannelHandlerContext ctx, UnixTime msg, ByteBuf out) {
+        out.writeInt((int)msg.value());
+    }
 }
