5fdd7c7300098a4b61d0ff11c1b2566666d4d3ce
jbeaken <jack747@gmail.com>
2020-09-26 12:14:29 +0100

    Formatting

--- src/main/java/org/jack/netty/client/TimeClient.java
+++ src/main/java/org/jack/netty/client/TimeClient.java
@@ -11,16 +11,18 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import org.jack.netty.handler.TimeClientHandler;
 
 public class TimeClient {
+    
     public static void main(String[] args) throws Exception {
         String host = args[0];
         int port = Integer.parseInt(args[1]);
+        
         EventLoopGroup workerGroup = new NioEventLoopGroup();
 
         try {
-            Bootstrap b = new Bootstrap(); // (1)
-            b.group(workerGroup); // (2)
-            b.channel(NioSocketChannel.class); // (3)
-            b.option(ChannelOption.SO_KEEPALIVE, true); // (4)
+            Bootstrap b = new Bootstrap(); 
+            b.group(workerGroup); 
+            b.channel(NioSocketChannel.class); 
+            b.option(ChannelOption.SO_KEEPALIVE, true); 
             b.handler(new ChannelInitializer<SocketChannel>() {
                 @Override
                 public void initChannel(SocketChannel ch) throws Exception {
@@ -29,7 +31,7 @@ public class TimeClient {
             });
 
             // Start the client.
-            ChannelFuture f = b.connect(host, port).sync(); // (5)
+            ChannelFuture f = b.connect(host, port).sync(); 
 
             // Wait until the connection is closed.
             f.channel().closeFuture().sync();
